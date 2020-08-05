dedc939d311b3842eb9a7b20721db2f50484fabb
Diego Taveras <diegotaveras2@outlook.com>
2020-08-04 21:29:25 -0400

    Adding style to the navigation

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import {Link} from 'gatsby';
+import styled from '@emotion/styled'
+
+const Nav = styled.nav`
+    display: flex;
+    flex-direction: column;
+    padding-bottom: 3rem;
+
+    @media (min-width: 768px){
+        padding: 0;
+        flex-flow: row;
+    }
+`;
+
+const NavLink = styled(Link)`
+    color: #FFF;
+    font-weight: 700;
+    font-family: 'Roboro' ,sans-serif;
+    text-decoration: none;
+    padding: .5rem;
+    &.actual-page{
+        border-bottom: 2px solid #FFF;
+    }
+`;
 
 const Navigation = () => {
     return (
-        <nav>
-            <Link to={'/'}>Home</Link>            
-            <Link to={'/properties'}>Properties</Link>            
-            <Link to={'/aboutus'}>About Us</Link>            
-        </nav>
+        <Nav>
+            <NavLink activeClassName='actual-page' to={'/'}>Home</NavLink>            
+            <NavLink activeClassName='actual-page' to={'/properties'}>Properties</NavLink>            
+            <NavLink activeClassName='actual-page' to={'/aboutus'}>About Us</NavLink>            
+        </Nav>
     );
 };
 
