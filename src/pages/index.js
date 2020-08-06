6aa1884ce598f5460925939a0a981ec43cca28b7
Diego Taveras <diegotaveras2@outlook.com>
2020-08-05 23:13:49 -0400

    gatsby-background-image component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,26 @@ import Layout from './../components/Layout';
 import useIndex from './../hooks/useIndex';
 import {css} from '@emotion/core';
 import styled from '@emotion/styled';
+import BackgroundImage from 'gatsby-background-image'
 
+const ImgBackground = styled(BackgroundImage)`
+    height: 600px;
+`;
 const Index = () => {
     const i = useIndex()
     const {name,content,img} = i[0]
     return (
         <Layout>
+            <ImgBackground
+                tag="section"
+                fluid={img.sharp.fluid}
+                fadeIn="soft"
+            >
+                <div>
+                    <h1></h1>
+                </div>
+
+            </ImgBackground>
             <main>
                 <div css={css`
                     max-width: 800px;
